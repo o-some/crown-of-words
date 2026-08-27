49d41c0e0dcadfa80ab4e2d92c60dd6739984818
o-some <eleftherios@o-some.de>
2026-08-27 19:06:33 +0200

    chore: remove temporary Branch 11 patch prep script

--- scripts/branch11-prep-patch.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'node:fs';
-const path='scripts/branch11-integrate.mjs';
-let text=fs.readFileSync(path,'utf8');
-text=text.replace('aria-label="Kronensiegel ${state.campaignComplete ?', 'aria-label="Kronensiegel \\${state.campaignComplete ?');
-text=text.replace('von 10">${state.campaignComplete ?', 'von 10">\\${state.campaignComplete ?');
-fs.writeFileSync(path,text);
