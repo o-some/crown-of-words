9e0c33a312304130fb8cb5ec3a8d1b218261bca7
o-some <eleftherios@o-some.de>
2026-08-27 19:06:41 +0200

    chore: remove temporary Branch 11 victory patch script

--- scripts/branch11-victory-save.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'node:fs';
-const path='src/main.js';let text=fs.readFileSync(path,'utf8');
-const needle='state.campaignComplete=Boolean(res.campaignComplete);if(res.campaignComplete){';
-const replacement='state.campaignComplete=Boolean(res.campaignComplete);saveStandaloneSave(createSaveEnvelope(state));if(res.campaignComplete){';
-const count=text.split(needle).length-1;if(count!==1)throw new Error(`victory save patch expected 1 match, got ${count}`);
-fs.writeFileSync(path,text.replace(needle,replacement));
